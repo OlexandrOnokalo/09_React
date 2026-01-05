606fdbf13de7ddcd619d0eaa31eccac3859488f4
OlexandrOnokalo <Onoka_nd96@student.itstep.org>
2026-01-05 20:16:37 +0200

    01_ HW done

--- 01_React_Intro/HW01/src/App.jsx
+++ 01_React_Intro/HW01/src/App.jsx
@@ -8,26 +8,11 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%BC%D1%96%D1%81%D1%82%D0%B0_%D0%A0%D1%96%D0%B2%D0%BD%D0%B5.svg/250px-%D0%9B%D0%BE%D0%B3%D0%BE%D1%82%D0%B8%D0%BF_%D0%BC%D1%96%D1%81%D1%82%D0%B0_%D0%A0%D1%96%D0%B2%D0%BD%D0%B5.svg.png" alt="Logo" />
+      <h1>Рівне</h1>
+      <p>Рі́вне — місто в Україні, обласний центр Рівненської області, адміністративний центр Рівненського району та Рівненської міської громади. Машинобудування, хімічна (ПАТ «Рівне-Азот»), легка, харчова промисловість, торфова промисловість. Три виші, два театри. Історико-краєзнавчий музей. Успенська дерев'яна церква (XVIII ст.).
+
+Відоме з 1283 року. Магдебурзьке право з 1492 року. Перша письмова згадка про Рівне у статусі міста — 1496 рік, у Волинському короткому літописі.</p>
     </>
   )
 }
